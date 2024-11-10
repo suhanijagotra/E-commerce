cda897086c90cf053e69f5328f8e1cf48fec6fda
Vanya Kataria <vanya@Vanyas-MacBook-Pro.local>
2024-11-10 13:36:05 +0530

    homepage 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client"; // Updated import for React 18
+import App from "./App";
+import ShopContextProvider from "./context/ShopContext";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css"; // Assuming you have a global CSS file
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+// Get the root DOM element
+const rootElement = document.getElementById("root");
+
+// Create the root
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the app
+root.render(
+  <React.StrictMode>
+    <ShopContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ShopContextProvider>
+  </React.StrictMode>
+);
