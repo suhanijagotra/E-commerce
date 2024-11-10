0e66de7c64ced7d14b5e20399eb263038994fe91
Suhani Jagotra <suhani.jagotra07@gmail.com>
2024-11-10 22:32:32 +0530

    pages rellocation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller';
-import OurPolicy from '../src/components/OurPolicy';
-import NewsletterBox from '../src/components/NewsletterBox';
+import OurPolicy from '../components/OurPolicy';
+import NewsletterBox from '../components/NewsletterBox';
 const Home = () => {
   return (
     <div>
