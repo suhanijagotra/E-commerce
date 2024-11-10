e44b790fc29993d70382712400bd5988649b8f09
Toshtosty <159453894+Toshtosty@users.noreply.github.com>
2024-11-10 15:04:43 +0000

    modified:   frontend/src/App.jsx         new file:   frontend/src/components/Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'> 
+
+
+
+        </div>
+
+
+
+    </div>
+  )
+}
+
+export default Footer
