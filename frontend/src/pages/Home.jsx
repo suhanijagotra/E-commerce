9f5e6ace481912957d6e351a30b5756e7cedb528
Suhani Jagotra <suhani.jagotra07@gmail.com>
2024-11-12 00:46:51 +0530

    frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
-import BestSeller from '../components/BestSeller';
-import OurPolicy from '../components/OurPolicy';
-import NewsletterBox from '../components/NewsletterBox';
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
+
 const Home = () => {
   return (
     <div>
       <Hero />
-      <LatestCollection />
+      <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
       <NewsletterBox/>
-      
     </div>
-  );
-};
+  )
+}
 
-export default Home;
-
+export default Home
