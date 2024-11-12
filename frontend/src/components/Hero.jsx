5057a0d6c99c3a078500b537948d4ae4b3c455c2
Suhani Jagotra <suhani.jagotra07@gmail.com>
2024-11-12 16:27:02 +0530

    s

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
         <div className='text-[#414141]'>
             <div className='flex items-center gap-2'>
-                <p className='w-8 md:w-11 h-[2px] bg-[414141]'></p>
+                <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                 <p className='font medium text-sm md:text-base'>OUR BESTSELLERS</p>
             </div>
             <h1 className='text-3xl sm:py-3 lg:text-5xl loading-relaxed prata-regular'> Latest Arrivals</h1>
